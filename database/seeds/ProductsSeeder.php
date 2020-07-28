06d792b1aeb0f3fcd489fc7a260624e546c4281d
Milan Lazic <kockastajabuka@gmail.com>
2020-07-28 23:37:31 +0000

    seeder update, image file names inserted into products

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -35,7 +35,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Margarita',
-            'image_url' => '',
+            'image_url' => 'margarita.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 5.4,
             'price_usd' => 4.64,
@@ -46,7 +46,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Megaroni',
-            'image_url' => '',
+            'image_url' => 'megaroni.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 6,
             'price_usd' => 5.16,
@@ -57,7 +57,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Hawaiian pizza',
-            'image_url' => '',
+            'image_url' => 'hawaiian_pizza.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 8.2,
             'price_usd' => 7.05,
@@ -68,7 +68,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Curry chicken pizza',
-            'image_url' => '',
+            'image_url' => 'curry_chicken_pizza.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 7.6,
             'price_usd' => 6.54,
@@ -79,7 +79,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Bacon and BBQ pizza',
-            'image_url' => '',
+            'image_url' => 'bacon_bbq_pizza.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 10.99,
             'price_usd' => 9.45,
@@ -90,7 +90,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Capricciosa',
-            'image_url' => '',
+            'image_url' => 'capricciosa.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 8,
             'price_usd' => 6.88,
@@ -101,7 +101,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Venezia',
-            'image_url' => '',
+            'image_url' => 'venezia.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 7.8,
             'price_usd' => 6.7,
@@ -112,7 +112,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Mexico',
-            'image_url' => '',
+            'image_url' => 'mexico.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 6.5,
             'price_usd' => 5.59,
@@ -123,7 +123,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Tuna fish pizza',
-            'image_url' => '',
+            'image_url' => 'tuna_fish_pizza.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 9.99,
             'price_usd' => 8.59,
@@ -134,7 +134,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 1,
             'name' => 'Calabrese',
-            'image_url' => '',
+            'image_url' => 'calabrese.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 5.8,
             'price_usd' => 4.98,
@@ -145,7 +145,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 2,
             'name' => 'Cola',
-            'image_url' => '',
+            'image_url' => 'cola.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 3,
             'price_usd' => 2.58,
@@ -156,7 +156,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 2,
             'name' => 'Orange soda',
-            'image_url' => '',
+            'image_url' => 'orange_soda.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 3,
             'price_usd' => 2.58,
@@ -167,7 +167,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 2,
             'name' => 'Lemonade',
-            'image_url' => '',
+            'image_url' => 'lemonade.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 3,
             'price_usd' => 2.58,
@@ -178,7 +178,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 3,
             'name' => 'Ice cream',
-            'image_url' => '',
+            'image_url' => 'ice_cream.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 4.5,
             'price_usd' => 3.87,
@@ -189,7 +189,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 3,
             'name' => 'Cake',
-            'image_url' => '',
+            'image_url' => 'cake.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 5,
             'price_usd' => 4.3,
@@ -200,7 +200,7 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'product_type_id' => 3,
             'name' => 'Pudding',
-            'image_url' => '',
+            'image_url' => 'pudding.png',
             'description' => 'Praesent sit amet eros odio. Donec arcu metus, aliquam nec accumsan eget, tristique bibendum eros. Aenean id dui quam. Morbi ullamcorper mauris ac lectus rhoncus fringilla. Vestibulum nec nunc turpis. Sed iaculis ligula eu nisl interdum, sed porta purus congue. Morbi venenatis pulvinar lorem.',
             'price_eur' => 3.5,
             'price_usd' => 3.01,
